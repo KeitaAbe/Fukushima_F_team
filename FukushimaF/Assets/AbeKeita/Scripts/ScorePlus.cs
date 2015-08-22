2b858755f44d17e84a79a4038a7a2472733cac85
KeitaAbe <example@example.com>
2015-08-23 02:50:00 +0900

    ボーナスキャラ設定済みプレハブ投下

---
+++ FukushimaF/Assets/AbeKeita/Scripts/ScorePlus.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charactorConroller : MonoBehaviour {
+	
+	private float angle = 0;
+	protected float speed = 0;
+	private float y = 0;
+	
+	void Update ()
+	{
+
+		float input = Input.GetAxisRaw ("Vertical");
+
+		if(input != 0){
+			y = input;
+			angle += y;
+		}
+
+		if(angle >= 90){
+			angle = 90;
+		}
+		else if(angle <= -90){
+			angle = -90;
+		}
+
+		speed = Mathf.Abs(Mathf.Cos(angle));
+		if(angle >= 90 || angle <= -90){
+			speed = 0;
+		}
+		
+		Vector2 direction = new Vector2(0, y);
+
+		GetComponent<Rigidbody2D>().rotation = angle;
+		GetComponent<Rigidbody2D>().velocity = speed * direction;
+	}
+}
