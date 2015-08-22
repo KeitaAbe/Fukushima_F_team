953a846d19acd49b5bae33ca27d7913504d24239
amane <kromurudo06@gmail.com>
2015-08-22 20:53:38 +0900

    邪魔者の動きの処理(仮)

---
+++ FukushimaF/Assets/Katayose/K_Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+	// : objポジションの取得変数.
+	private Vector3 obj_Pos ;
+	// : 時間の取得変数.
+	private float   time  ;
+	public  float 	speed ;
+
+
+	void Start ()
+	{
+		// : ポジションの取得.
+		obj_Pos = this.transform.position ;
+		time = 0.0f ;
+	}
+
+	void Update ()
+	{
+		runEnemy() ;
+	}
+
+
+	public void runEnemy()
+	{
+		// : 時間の取得.
+		time += Time.deltaTime ;
+		// : 経過時間に対応した移動・反映.
+		obj_Pos.x += time * -speed ;
+		this.transform.position = obj_Pos ;	
+	}
+}
---
+++ FukushimaF/Assets/Katayose/K_Scripts/Fish.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish : MonoBehaviour
+{
+	private bool  jumpFlag = true ;
+
+	public  float thrust_UP ;
+	public  float thrust_LEFT ;
+	
+	void Update ()
+	{
+		if( jumpFlag )
+		{
+			GetComponent<Rigidbody2D>().AddForce(Vector2.up   * thrust_UP) ;
+			GetComponent<Rigidbody2D>().AddForce(Vector2.left * thrust_LEFT) ;
+			jumpFlag = false ;
+		}
+	}
+}
