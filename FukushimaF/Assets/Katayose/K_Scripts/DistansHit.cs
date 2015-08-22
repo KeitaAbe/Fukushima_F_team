88f6918388c1f7ac927f6e5aebb796ccd6ca3ae6
amane <kromurudo06@gmail.com>
2015-08-22 22:54:07 +0900

    魚と風船のPrefab

---
+++ FukushimaF/Assets/Katayose/K_Scripts/DistansHit.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistansHit : MonoBehaviour
+{
+	public GameObject icarus ;
+
+	private Vector2 ica_Pos ;
+	private float ica_RD ;
+
+	private Vector2 pos  ;
+	private float radius ;
+
+	private float difference ;
+
+	void Start ()
+	{
+		pos = this.transform.position ;
+		radius = this.transform.position.x / 2 ;
+
+		if( !icarus )
+		{
+			icarus = GameObject.Find("Quad") ;
+
+			ica_Pos = icarus.transform.position ;
+			ica_RD = icarus.transform.localScale.x / 2 ;
+		}
+	}
+	
+	void Update ()
+	{
+		difference = Vector2.Distance(ica_Pos, pos) ;
+
+		if( difference >= 3.0f )
+		{
+			Debug.Log("d") ;
+		}
+	}
+}
