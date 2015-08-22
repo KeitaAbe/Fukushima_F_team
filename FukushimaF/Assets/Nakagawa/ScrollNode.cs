ff90ca88fe1b3ff093668a3ee369a6fd382a258b
nyakagawan <isoidemasu@gmail.com>
2015-08-22 19:21:18 +0900

    add nakagawa workspace

---
+++ FukushimaF/Assets/Nakagawa/ScrollNode.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScrollNode : MonoBehaviour {
+    public float _scrollSpeed = 0f;
+    Vector3 _initialPos = Vector3.zero;
+
+    void Awake()
+    {
+    }
+
+	// Use this for initialization
+	void Start () {
+        ScrollController.instance.AddNode(this);
+        _initialPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var newPos = transform.localPosition;
+        newPos.x += _scrollSpeed * Time.smoothDeltaTime;
+        transform.localPosition = newPos;
+	}
+
+    public void ArrivedEndPoint()
+    {
+        var newPos = _initialPos;
+        newPos.x = ScrollController.instance.scrollStartLocator.position.x;
+        transform.position = newPos;
+    }
+}
