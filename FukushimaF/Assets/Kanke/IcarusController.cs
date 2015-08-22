ea269a042fc9b18d5276fca1d5c2753824231b5b
Toshikazu Kanke <ToshikazuKanke@kanke-no-MacBook-Pro.local>
2015-08-23 03:36:14 +0900

    SpriteStudio版イカロス
    いろいろ変えちゃいました

---
+++ FukushimaF/Assets/Kanke/IcarusController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class IcarusController : MonoBehaviour {
+	
+	public float speed;
+	public float speedAngle;
+	private float angle = 0;
+	
+	void Start ()
+	{
+		
+	}
+
+	void Update ()
+	{
+
+		float input = Input.GetAxisRaw ("Vertical");
+		
+		angle += speedAngle * input * Time.deltaTime;
+
+		angle = Mathf.Max( -90f, Mathf.Min( 90f, angle ) );
+
+		Vector3 verocity = new Vector3(
+			speed * Mathf.Cos( angle * Mathf.Deg2Rad ) * 0f,
+			speed * Mathf.Sin( angle * Mathf.Deg2Rad ),
+			0f
+		);
+		
+		Vector3 angles = new Vector3( 0f, 0f, angle );
+
+		transform.localEulerAngles = angles;
+		transform.position += verocity;
+	}	
+}
---
+++ FukushimaF/Assets/Kanke/IcarusController_partial.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public partial class IcarusController : MonoBehaviour {
+    public float rightVelocity
+    {
+        get
+        {
+			return speed * Mathf.Cos( angle * Mathf.Deg2Rad );
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Chara Angle:" + angle);
+        GUILayout.Label("Chara Speed:" + speed);
+        GUILayout.Label("Chara rightVel:" + rightVelocity);
+    }
+}
