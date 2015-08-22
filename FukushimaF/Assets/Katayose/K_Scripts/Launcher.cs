2bc85636d1355ed341f3b912c441fbb691f8a2d3
amane <kromurudo06@gmail.com>
2015-08-22 18:07:23 +0900

    敵の配置情報

---
+++ FukushimaF/Assets/Katayose/K_Scripts/Launcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour
+{
+	
+	private Vector3 obj_Pos ;
+	private float   time  ;
+
+	public  float 	speed ;
+
+	void Start ()
+	{
+		obj_Pos = this.transform.position ;
+		time = 0.0f ;
+	}
+
+	void Update ()
+	{
+		time = Time.deltaTime ;
+
+		obj_Pos.x += time * -1 * speed ;
+		this.transform.position = obj_Pos ;
+	}
+}
---
+++ FukushimaF/Assets/Katayose/K_Scripts/Spawn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour
+{
+	// : 発射オブジェクト.
+	public GameObject[] obj ;
+
+	private int rand ;
+
+	void Start ()
+	{
+		rand = Random.Range(0, 4) ;
+
+		switch( rand )
+		{
+			case 0 :
+				Instantiate( obj[0], this.transform.position, Quaternion.identity ) ;
+				break ;
+
+			case 1 :
+				Instantiate( obj[0], this.transform.position, Quaternion.identity ) ;
+				break ;
+
+			case 2 :
+				Instantiate( obj[0], this.transform.position, Quaternion.identity ) ;
+				break ;
+
+			case 3 :
+				Instantiate( obj[1], this.transform.position, Quaternion.identity ) ;
+				break ;				
+		}
+	}
+}
