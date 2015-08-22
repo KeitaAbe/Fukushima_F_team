d4aaf6f7cdc973fd224792d5ede4c38933da2a5a
fujinaka <luvresetal@gmail.com>
2015-08-22 21:50:02 +0900

    イカロス君の挙動
    暫定版をコミットします。

---
+++ FukushimaF/Assets/Naito/charactorConroller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class charactorConroller : MonoBehaviour {
+	
+	private float angle = 0;
+	protected float speed = 9;
+	private float y = 0;
+	
+	void Update ()
+	{
+
+		float input = Input.GetAxisRaw ("Vertical");
+
+		if(input != 0){
+			y = input;
+			angle += y;
+			Debug.Log(y);
+		}
+
+		if(angle >= 90){
+			angle = 90;
+		}
+		else if(angle <= -90){
+			angle = -90;
+		}
+		Debug.Log(angle);
+		speed = -(Mathf.Abs(angle) - 90) / 50;
+
+	/*	if(speed < 0){
+			speed = 0.1f;
+		}*/
+		//角度の絶対値が大きければスピードは遅い
+
+
+		Vector2 direction = new Vector2(0, y);
+
+		GetComponent<Rigidbody2D>().rotation = angle;
+		GetComponent<Rigidbody2D>().velocity = speed * direction;
+	}
+}
