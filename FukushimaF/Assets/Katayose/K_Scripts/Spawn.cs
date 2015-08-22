7cc77d465a7ff55371d8c9569b9f9ffaa83da3e1
amane <kromurudo06@gmail.com>
2015-08-23 05:59:46 +0900

    敵の出現調整

--- FukushimaF/Assets/Katayose/K_Scripts/Spawn.cs
+++ FukushimaF/Assets/Katayose/K_Scripts/Spawn.cs
@@ -8,28 +8,38 @@ public class Spawn : MonoBehaviour
 
 	public int rand ;
 
-	void Start ()
+	private bool isInstan = false ;
+
+	void Update ()
 	{
 		rand = Random.Range(0, obj.Length) ;
 
         GameObject gobj = null;
-		switch( rand )
+
+        float pos = this.transform.position.x ;
+		
+		if( pos < 6.0f && !isInstan )
 		{
-			case 0 :
-                gobj = Instantiate<GameObject>(obj[0]);
-				break ;
+			switch( rand )
+			{
+				case 0 :
+	                gobj = Instantiate<GameObject>(obj[0]);
+					break ;
 
-			case 1 :
-                gobj = Instantiate<GameObject>(obj[1]);
-				break ;
+				case 1 :
+	                gobj = Instantiate<GameObject>(obj[1]);
+					break ;
 
-			case 2 :
-                gobj = Instantiate<GameObject>(obj[2]);
-				break ;
+				case 2 :
+	                gobj = Instantiate<GameObject>(obj[2]);
+					break ;
 
-			case 3 :
-                gobj = Instantiate<GameObject>(obj[0]);
-				break ;				
+				case 3 :
+	                gobj = Instantiate<GameObject>(obj[0]);
+					break ;				
+			}
+
+			isInstan = true ;
 		}
 
         if (gobj != null)
