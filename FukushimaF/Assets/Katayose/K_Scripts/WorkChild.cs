b227eb3526f41b3c59b98e91056a1ad25bf3f299
amane <kromurudo06@gmail.com>
2015-08-23 11:31:19 +0900

    くるくる回転

---
+++ FukushimaF/Assets/Katayose/K_Scripts/WorkChild.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorkChild : MonoBehaviour
+{
+	Animator animator ;
+	float time = 0.0f ;
+	bool  isColl = false ;
+
+	void Start ()
+	{
+		animator = GetComponent<Animator>() ;
+		animator.speed = 0.0f ;
+	}
+
+	void Update ()
+	{
+		if( isColl )
+		{
+			animator.speed = 1.0f ;
+
+			time += Time.deltaTime ;
+
+			if( time >= 1.0f )
+			{
+				animator.speed = 0.0f ;
+				Destroy(this.gameObject) ;
+			}
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if( coll.tag == "Player" )
+		{
+			isColl = true ;
+		}
+	}
+}
